473616dfd5d32ff380e734886dc37b64b27c9c3b
frankadear <63919741+frankadear@users.noreply.github.com>
2021-07-07 12:20:37 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$hostname = "sql102.epizy.com";
+#$username = "akhfqkli_login_register_youtube";
+$username = "epiz_29052248";
+#$password = "_RxF9!NO?mzv";
+$password = "fckESsxBCZhr";
+#$database = "akhfqkli_login_register_youtube";
+$database = "epiz_29052248_cars_project";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+#$base_url = "https://code.akhfasoft.net/login-register-youtube/";
+
+
+
+?>
